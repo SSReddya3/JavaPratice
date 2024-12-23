b84e21de248c3769ce309ecc4b1786b264e1b595
SSReddya3 <ssreddy.a3@gmail.com>
2024-12-23 15:39:01 +0530

    adding chart At

---
+++ src/test/java/javaPractice/D_ChartAt.java
@@ -0,0 +1,29 @@
+package javaPractice;
+
+public class D_ChartAt {
+
+	public static void main(String[] args) {
+		
+		String s="Siva Sankar Reddy Ankireddypalle";
+		
+		for(int i=0;i<s.length();i++) {
+			System.out.println(s.charAt(i));
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+	private static int lenth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
